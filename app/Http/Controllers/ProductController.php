8df933835a77130654ff52f6884615a88872d8bd
Jennifer <jeante05@gmail.com>
2022-09-13 00:22:34 -0500

    feat: update controllerProduct function addProductOrder

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Order\StoreOrderActions;
+use App\Actions\Order\UpdateOrderActions;
+use App\Actions\OrderProduct\StoreUpdateOrderProductActions;
 use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 /**
@@ -22,6 +26,17 @@ class ProductController extends Controller
 
         return view('product.index', compact('products'))
             ->with('i', (request()->input('page', 1) - 1) * $products->perPage());
+    }
+
+    public function addProductOrder (Product $product): RedirectResponse
+    {
+        $order = StoreOrderActions::execute();
+
+        $orderProduct = StoreUpdateOrderProductActions::execute($order, $product);
+
+        $orderUpdate = UpdateOrderActions::execute($order);
+
+        return redirect()->route('products.index')->with('success', 'Product Update successfully.');
     }
 
 
