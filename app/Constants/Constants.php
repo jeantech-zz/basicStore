8f781aa0697f8b517574510797c72b91a76aae1a
Jennifer <jeante05@gmail.com>
2022-09-12 21:30:56 -0500

    feat: update constants add placetopay

--- app/Constants/Constants.php
+++ app/Constants/Constants.php
@@ -12,4 +12,15 @@ final class Constants
     public const STATUS_ORDER_PAYED = 'PAYED';
     public const STATUS_ORDER_REJECTED = 'REJECTED';
     public const CURRENCY = 'COP';
+    public const IP_ADDRESS_PLACETOPAY =  '127.0.0.1';
+    public const USER_AGENT_PLACETOPAY =  'PlacetoPay Sandbox';
+    public const URL_PLACETOPAY ='https://dev.placetopay.com/redirection/api/session';
+    public const LOGIN_PLACETOPAY = '6dd490faf9cb87a9862245da41170ff2';
+
+    public const URL_RETURN_PLACETOPAY = 'http://127.0.0.1:8000/orders';
+    public const DESCRIPTION_PLACETOPAY = 'Pago Merca todo Correspondiente a la orden No.';
+    public const STATUS_ORDER_INPROCESSPAY = 'INPROCESSPAY';
+
 }
+
+
