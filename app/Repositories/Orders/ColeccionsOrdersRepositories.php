42f6bd1a4d5a12ed171a1df076c8f80812f77791
Jennifer <jeante05@gmail.com>
2022-09-13 16:24:20 -0500

    fix: update class reposetorioOrder

--- app/Repositories/Orders/ColeccionsOrdersRepositories.php
+++ app/Repositories/Orders/ColeccionsOrdersRepositories.php
@@ -11,7 +11,7 @@ class ColeccionsOrdersRepositories implements OrdersRepositories
         return Order::paginate();
     }
 
-    public function orderId (Order $order)
+    public function orderId (Order $order):Order
     {
        return Order::select('orders.*', 'users.name As userName')
         ->join('users', 'orders.user_id', '=', 'users.id')
@@ -19,14 +19,20 @@ class ColeccionsOrdersRepositories implements OrdersRepositories
         ->first();
     }
 
-    public function requestOrder (int $id)
+    public function requestOrder(int $id):Order
     {
-
         return   Order::select('orders.*', 'requests.processUrl As processUrl')
         ->join('requests', 'requests.order_id', '=', 'orders.id')
         ->where('orders.id',$id)
         ->first();
     }
 
+    public function order(int $idOrder):Order
+    {
+       return Order::select('orders.*', 'users.name As userName')
+        ->join('users', 'orders.user_id', '=', 'users.id')
+        ->where('orders.id',$idOrder)
+        ->first();
+    }
 
 }
