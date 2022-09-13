c348c0bb0d51a38d6d50e17eaebe909ff4f10f40
Jennifer <jeante05@gmail.com>
2022-09-12 23:18:02 -0500

    feat: creation storeOrderActions and test

---
+++ app/Constants/Constants.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Constants;
+
+/**
+ * Class Constant.
+ */
+final class Constants
+{
+    public const STATUS_ORDER_INPROCESS = 'INPROCESS';
+    public const STATUS_ORDER_CREATED = 'CREATED';
+    public const STATUS_ORDER_PAYED = 'PAYED';
+    public const STATUS_ORDER_REJECTED = 'REJECTED';
+    public const CURRENCY = 'COP';
+}
--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Actions\Order\StoreOrderActions;
+use App\Constants\Constants;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Config;
 
 class OrderTest extends TestCase
 {
@@ -13,5 +16,14 @@ class OrderTest extends TestCase
         $response = $this->get('orders');
 
         $response->assertStatus(200);
+    }
+
+    public function test_create_action_order()
+    {
+        $order = StoreOrderActions::execute();
+
+        $this->assertSame((int)$order->total, 1);
+        $this->assertSame((string)$order->status, "INPROCESS");
+        $this->assertSame((string)$order->currency,"COP");
     }
 }
