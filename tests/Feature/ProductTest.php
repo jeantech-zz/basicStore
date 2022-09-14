105da697e7b7b7a99f8bbad9b355c2984f34b6f5
Jennifer <jeante05@gmail.com>
2022-09-14 16:08:37 -0500

    fix: test userAct

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Actions\Order\StoreOrderActions;
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -12,7 +13,8 @@ class ProductTest extends TestCase
 {
     public function test_product_screen_can_be_rendered(): void
     {
-        $response = $this->get('products/index');
+        $user = User::factory()->create();
+        $response =$this->actingAs($user)->get(route('products.index'));
 
         $response->assertStatus(200);
     }
@@ -20,7 +22,8 @@ class ProductTest extends TestCase
     public function test_add_product_order_screen_can_be_rendered()
     {
         $product = Product::factory()->create();
-        $response = $this->post('products/addProductOrder/' . $product->id);
+        $user = User::factory()->create();
+        $response =$this->actingAs($user)->post('products/addProductOrder/' . $product->id);
 
         $response->assertRedirect(route('orders.index'));
     }
