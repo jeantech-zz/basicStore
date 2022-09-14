d11bc7bf3c6cc6156ddbb2513890094b7e61037a
Jennifer <jeante05@gmail.com>
2022-09-14 15:04:09 -0500

    fix: status creation order

--- app/Actions/Order/StoreOrderActions.php
+++ app/Actions/Order/StoreOrderActions.php
@@ -4,13 +4,12 @@ namespace App\Actions\Order;
 
 use App\Constants\Constants;
 use App\Models\Order;
-use Illuminate\Support\Facades\Config;
 
 class StoreOrderActions
 {
     public static function execute($userId): Order
     {
-        $statusInprocess =  Constants::STATUS_ORDER_INPROCESS;
+        $statusInprocess =  Constants::STATUS_ORDER_CREATED;
         $currency = Constants::CURRENCY;
 
         $order = Order::Where('user_id', auth()->id())
